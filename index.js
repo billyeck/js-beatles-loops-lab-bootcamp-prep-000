69993db3c47893e11557520853e1c8c691a71b45
William Eck <billyeck@gmail.com>
2019-04-17 19:40:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 var a=[];
 function theBeatlesPlay(musicians, instruments){
-  for (i=0; i<musicians.length; i++){
+  for (var i=0; i<musicians.length; i++){
     a.push(musicians[i]+" plays "+instruments[i]);
   }
 }
